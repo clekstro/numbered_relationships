234b4f1b31d07edf11d5bd8372340b7264008017
frosch62 <curtisekstrom@gmail.com>
2012-09-05 10:18:53 +0200

    add without class filter tests

--- spec/dummy/spec/support/shared_example_group_has_many_through_association.rb
+++ spec/dummy/spec/support/shared_example_group_has_many_through_association.rb
@@ -2,7 +2,6 @@ shared_examples_for "a class based through association filter" do |klass, thru_m
   #klass, thru_model, association_type, factory_name
   #Jester, :repertoire, :performances, :performance
   let(:instance_with_more){ FactoryGirl.create(klass.name.downcase.to_sym) }
-  let(:instance_with_less){ FactoryGirl.create(klass.name.downcase.to_sym) }
   context "with_at_least" do
     it "should respond with empty array when no has_many objects" do
       klass.with_at_least(1, association_type).should == []
@@ -55,7 +54,6 @@ shared_examples_for "a class based through association filter" do |klass, thru_m
       klass.with_exactly(1, association_type).should == []
     end
     it "should not show jesters with more or less than n" do
-      klass.with_exactly(1, association_type).should_not include(instance_with_less)
       j1 = thru_model.to_s.classify.constantize.create!
       k1 = FactoryGirl.create(factory_name)
       eval("instance_with_more.#{thru_model.to_s} = j1")
@@ -73,20 +71,21 @@ shared_examples_for "a class based through association filter" do |klass, thru_m
       klass.with_exactly(1, association_type).should include(instance_with_more)
     end
   end
-  # context "without" do
-  #   it "should not return objects with n jokes" do
-  #     klass.without(0, association_type).should_not include(instance_with_more, instance_with_less)
-  #   end
-  #   it "should return objects with non n factory_name counts" do
-  #     j1 = FactoryGirl.create(factory_name)
-  #     j2 = FactoryGirl.create(factory_name)
-  #     j3 = FactoryGirl.create(factory_name)
-  #     eval("instance_with_more.#{thru_model.to_s}.#{association_type.to_s} << [j1, j2, j3]")
-  #     j4 = FactoryGirl.create(factory_name)
-  #     eval("instance_with_less.#{association_type.to_s} << j4")
-  #     klass.without(2, association_type).should include(instance_with_more, instance_with_less)
-  #   end
-  # end
+  context "without" do
+    it "should not return objects with n" do
+      klass.without(1, association_type).should_not include(instance_with_more)
+    end
+    it "should return objects with non n amounts" do
+      j1 = thru_model.to_s.classify.constantize.create!
+      k1 = FactoryGirl.create(factory_name)
+      eval("instance_with_more.#{thru_model.to_s} = j1")
+      eval("instance_with_more.#{thru_model.to_s}.#{association_type.to_s} << [k1]")
+      klass.without(2, association_type).should include(instance_with_more)
+      k2 = FactoryGirl.create(factory_name)
+      eval("instance_with_more.#{thru_model.to_s}.#{association_type.to_s} << [k2]")
+      klass.without(1, association_type).should include(instance_with_more)
+    end
+  end
   # context "with_more_than" do
   #   it "should not return objects with less than or equal to n jokes" do
   #     klass.with_more_than(0, association_type).should_not include(instance_with_more, instance_with_less)
