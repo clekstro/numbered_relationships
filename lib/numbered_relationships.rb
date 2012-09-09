7d32c153eb21a65d649d9180639a200c0ea9402a
Curtis Ekstrom <ce@canvus.io>
2012-09-10 01:01:44 +0200

    remove unnecessary methods and add necessary reference to test model

--- lib/numbered_relationships.rb
+++ lib/numbered_relationships.rb
@@ -1,9 +1,10 @@
+# require_relative './numbered_relationships/query_constructor'
+# require_relative './numbered_relationships/query_executer'
+
 module NumberedRelationships
   module AmountFilters
   	extend ActiveSupport::Concern
-  	included do
-  	end
-  		
+
 		module ClassMethods  	
 	    def with_at_least(n, assoc)
 	      find_related_objects(n, assoc, '>=')
@@ -25,45 +26,16 @@ module NumberedRelationships
 	    end
 
 	    private
+
 	    def find_related_objects(n, assoc, operator)
-	      called_on_klass = self.instance_of?(Class)
-	      reflection = determine_reflection(called_on_klass, assoc)
-	      return [] if reflection.nil?
-	      table = determine_table(called_on_klass, reflection)
-	      #puts table
+	      reflection = self.reflect_on_association(assoc)
+	      return [] unless reflection
+	      table = self.name.tableize
 	      query_related_objects(reflection, n, operator, table).scoped
 	    end
 
-	    def determine_reflection(called_on_klass, assoc)
-	      return self.reflect_on_association(assoc) if called_on_klass
-	      klass = self.proxy_association.klass
-	      klass.reflect_on_association(assoc)
-	    end
-	    def determine_table(called_on_klass, reflection)
-	      macro = reflection.macro
-	      case macro
-	      when :has_and_belongs_to_many
-	        determine_habtm_table(called_on_klass, reflection)
-	      when :has_many
-	        determine_has_many_table(called_on_klass, reflection)
-	      end
-	    end
-
-	    def determine_habtm_table(called_on_klass, reflection)
-	      return self.name.tableize if called_on_klass
-	      reflection.options[:join_table]
-	    end
-
-	    def determine_has_many_table(called_on_klass, reflection)
-	      # distinguish between has_many and has_many :through
-	      return self.name.tableize if called_on_klass
-	      reflection.options[:join_table]
-	    end
-
 	    def query_related_objects(reflection, n, operator, table)
-	      macro = reflection.macro
-	      #puts "Macro is: " + macro.to_s
-	      case macro
+	      case reflection.macro
 	      when :has_and_belongs_to_many
 	        construct_and_execute_habtm_query(reflection, n, operator, table)
 	      when :has_many
@@ -74,15 +46,16 @@ module NumberedRelationships
 	    def construct_and_execute_habtm_query(reflection, n, operator, table)
 	      foreign_key = reflection.foreign_key
 	      association = reflection.name
+	      join_table = self.reflect_on_association(association.to_sym).options[:join_table]
+	      return self.joins(association).group("#{table}.id HAVING count(#{join_table}.#{foreign_key}) #{operator} #{n}") if join_table
 	      self.joins(association).group("#{table}.id HAVING count(#{foreign_key}) #{operator} #{n}")
 	    end
 	    #Artist.joins(:artist_portfolio, :artworks).group("artists.id HAVING count(artworks.id) > 2")
 	    def construct_and_execute_has_many_through_query(reflection, n, operator, table)
-	      foreign_key = reflection.foreign_key
+	      foreign_key = reflection.foreign_key	      
 	      association = reflection.name
 	      through_model = reflection.options[:through]
-	      self.joins(through_model, association)
-	      .group("#{table}.id HAVING count(#{association.to_s}.id) #{operator} #{n}")
+	      self.joins(through_model, association).group("#{table}.id HAVING count(#{association.to_s}.id) #{operator} #{n}")
 	    end
 	    def construct_and_execute_has_many_query(reflection, n, operator, table)
 	      foreign_key = reflection.foreign_key
@@ -91,11 +64,6 @@ module NumberedRelationships
 	    end
 	  end
   end
-
-  # def self.included(receiver)
-  #   receiver.extend         ClassMethods
-  #   receiver.send :include, InstanceMethods
-  # end
 end
 
 ActiveRecord::Base.send :include, NumberedRelationships::AmountFilters
--- spec/dummy/app/models/artistic_pause.rb
+++ spec/dummy/app/models/artistic_pause.rb
@@ -1,4 +1,5 @@
 class ArtisticPause < ActiveRecord::Base
   attr_accessible :duration, :performance_id
   belongs_to :performance
+  has_many :dramatic_moments
 end
