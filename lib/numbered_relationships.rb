12d0654bd21fb679c2c0de9bb76139e68a0a09b1
Curtis Ekstrom <ce@canvus.io>
2012-09-10 01:56:56 +0200

    fix formatting, remove cruft

--- lib/numbered_relationships.rb
+++ lib/numbered_relationships.rb
@@ -1,68 +1,65 @@
-# require_relative './numbered_relationships/query_constructor'
-# require_relative './numbered_relationships/query_executer'
-
 module NumberedRelationships
   module AmountFilters
-  	extend ActiveSupport::Concern
+    extend ActiveSupport::Concern
 
-		module ClassMethods  	
-	    def with_at_least(n, assoc)
-	      find_related_objects(n, assoc, '>=')
-	    end
-	    def with_at_most(n, assoc)
-	      find_related_objects(n, assoc, '<=')
-	    end
-	    def with_exactly(n, assoc)
-	      find_related_objects(n, assoc, '=')
-	    end
-	    def without(n, assoc)
-	      find_related_objects(n, assoc, '<>')
-	    end
-	    def with_more_than(n, assoc)
-	      find_related_objects(n, assoc, '>')
-	    end
-	    def with_less_than(n, assoc)
-	      find_related_objects(n, assoc, '<')
-	    end
+    module ClassMethods
+      def with_at_least(n, assoc)
+        find_related_objects(n, assoc, '>=')
+      end
+      def with_at_most(n, assoc)
+        find_related_objects(n, assoc, '<=')
+      end
+      def with_exactly(n, assoc)
+        find_related_objects(n, assoc, '=')
+      end
+      def without(n, assoc)
+        find_related_objects(n, assoc, '<>')
+      end
+      def with_more_than(n, assoc)
+        find_related_objects(n, assoc, '>')
+      end
+      def with_less_than(n, assoc)
+        find_related_objects(n, assoc, '<')
+      end
 
-	    private
+      private
 
-	    def find_related_objects(n, assoc, operator)
-	      reflection = self.reflect_on_association(assoc)
-	      return [] unless reflection
-	      table = self.name.tableize
-	      query_related_objects(reflection, n, operator, table).scoped
-	    end
+      def find_related_objects(n, assoc, operator)
+        reflection = self.reflect_on_association(assoc)
+        return [] unless reflection
+        table = self.name.tableize
+        query_related_objects(reflection, n, operator, table).scoped
+      end
 
-	    def query_related_objects(reflection, n, operator, table)
-	      case reflection.macro
-	      when :has_and_belongs_to_many
-	        construct_and_execute_habtm_query(reflection, n, operator, table)
-	      when :has_many
-	        return construct_and_execute_has_many_through_query(reflection, n, operator, table) if reflection.options[:through]
-	        construct_and_execute_has_many_query(reflection, n, operator, table)
-	      end
-	    end
-	    def construct_and_execute_habtm_query(reflection, n, operator, table)
-	      foreign_key = reflection.foreign_key
-	      association = reflection.name
-	      join_table = self.reflect_on_association(association.to_sym).options[:join_table]
-	      return self.joins(association).group("#{table}.id HAVING count(#{join_table}.#{foreign_key}) #{operator} #{n}") if join_table
-	      self.joins(association).group("#{table}.id HAVING count(#{foreign_key}) #{operator} #{n}")
-	    end
-	    #Artist.joins(:artist_portfolio, :artworks).group("artists.id HAVING count(artworks.id) > 2")
-	    def construct_and_execute_has_many_through_query(reflection, n, operator, table)
-	      foreign_key = reflection.foreign_key	      
-	      association = reflection.name
-	      through_model = reflection.options[:through]
-	      self.joins(through_model, association).group("#{table}.id HAVING count(#{association.to_s}.id) #{operator} #{n}")
-	    end
-	    def construct_and_execute_has_many_query(reflection, n, operator, table)
-	      foreign_key = reflection.foreign_key
-	      association = reflection.name
-	      self.joins(association).group("#{table}.id HAVING count(#{association.to_s}.id) #{operator} #{n}")
-	    end
-	  end
+      def query_related_objects(reflection, n, operator, table)
+        case reflection.macro
+        when :has_and_belongs_to_many
+          construct_and_execute_habtm_query(reflection, n, operator, table)
+        when :has_many
+          return construct_and_execute_has_many_through_query(reflection, n, operator, table) if reflection.options[:through]
+          construct_and_execute_has_many_query(reflection, n, operator, table)
+        end
+      end
+      def construct_and_execute_habtm_query(reflection, n, operator, table)
+        foreign_key = reflection.foreign_key
+        association = reflection.name
+        join_table = self.reflect_on_association(association.to_sym).options[:join_table]
+        return self.joins(association).group("#{table}.id HAVING count(#{join_table}.#{foreign_key}) #{operator} #{n}") if join_table
+        self.joins(association).group("#{table}.id HAVING count(#{foreign_key}) #{operator} #{n}")
+      end
+      #Artist.joins(:artist_portfolio, :artworks).group("artists.id HAVING count(artworks.id) > 2")
+      def construct_and_execute_has_many_through_query(reflection, n, operator, table)
+        foreign_key = reflection.foreign_key
+        association = reflection.name
+        through_model = reflection.options[:through]
+        self.joins(through_model, association).group("#{table}.id HAVING count(#{association.to_s}.id) #{operator} #{n}")
+      end
+      def construct_and_execute_has_many_query(reflection, n, operator, table)
+        foreign_key = reflection.foreign_key
+        association = reflection.name
+        self.joins(association).group("#{table}.id HAVING count(#{association.to_s}.id) #{operator} #{n}")
+      end
+    end
   end
 end
 
