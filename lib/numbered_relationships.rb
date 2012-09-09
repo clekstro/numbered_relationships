db8a5ce97ff0287ab032060ea982aa88b13045eb
Curtis Ekstrom <ce@canvus.io>
2012-09-10 01:58:51 +0200

    fix formatting, remove cruft #2

--- lib/numbered_relationships.rb
+++ lib/numbered_relationships.rb
@@ -6,18 +6,23 @@ module NumberedRelationships
       def with_at_least(n, assoc)
         find_related_objects(n, assoc, '>=')
       end
+
       def with_at_most(n, assoc)
         find_related_objects(n, assoc, '<=')
       end
+
       def with_exactly(n, assoc)
         find_related_objects(n, assoc, '=')
       end
+
       def without(n, assoc)
         find_related_objects(n, assoc, '<>')
       end
+
       def with_more_than(n, assoc)
         find_related_objects(n, assoc, '>')
       end
+      
       def with_less_than(n, assoc)
         find_related_objects(n, assoc, '<')
       end
@@ -40,6 +45,7 @@ module NumberedRelationships
           construct_and_execute_has_many_query(reflection, n, operator, table)
         end
       end
+
       def construct_and_execute_habtm_query(reflection, n, operator, table)
         foreign_key = reflection.foreign_key
         association = reflection.name
@@ -47,13 +53,14 @@ module NumberedRelationships
         return self.joins(association).group("#{table}.id HAVING count(#{join_table}.#{foreign_key}) #{operator} #{n}") if join_table
         self.joins(association).group("#{table}.id HAVING count(#{foreign_key}) #{operator} #{n}")
       end
-      #Artist.joins(:artist_portfolio, :artworks).group("artists.id HAVING count(artworks.id) > 2")
+
       def construct_and_execute_has_many_through_query(reflection, n, operator, table)
         foreign_key = reflection.foreign_key
         association = reflection.name
         through_model = reflection.options[:through]
         self.joins(through_model, association).group("#{table}.id HAVING count(#{association.to_s}.id) #{operator} #{n}")
       end
+
       def construct_and_execute_has_many_query(reflection, n, operator, table)
         foreign_key = reflection.foreign_key
         association = reflection.name
